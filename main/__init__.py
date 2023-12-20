0b78021957d0416fb1002ba23df6a10e3259197a
mlbtvps <119069067+mlbtvps@users.noreply.github.com>
2023-12-20 22:47:45 +0630

    Ok

--- main/__init__.py
+++ main/__init__.py
@@ -18,6 +18,7 @@ BOT_TOKEN = config("BOT_TOKEN", default=None)
 SESSION = config("SESSION", default=None)
 FORCESUB = config("FORCESUB", default=None)
 AUTH = config("AUTH", default=None, cast=int)
+DB_CHANNEL = config("DB_CHANNEL", default=None)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
